1d310cb1b4e692d3d5c9c32ea00584b9f8e68fc2
Kikadass <kikadass@gmail.com>
2017-10-08 15:34:38 +0100

    First Commit
    Image Analisis Project for university.
    1st step: being able to take away noise from the picture

---
+++ cmake-build-debug/CMakeFiles/ImageAnalisis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageAnalisis.dir/main.cpp.o"
+  "ImageAnalisis.pdb"
+  "ImageAnalisis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageAnalisis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ImageAnalisis.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ImageAnalisis.
+# This may be replaced when dependencies are built.
