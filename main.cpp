d6a5caa5d471773c95479cb06ea4fc23589d7d28
Kikadass <kikadass@gmail.com>
2017-11-02 22:24:22 +0000

    added band pass, lowpass and high pass

--- main.cpp
+++ main.cpp
@@ -29,6 +29,14 @@ double distance(double x1, double y1, double x2, double y2) {
     return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
 
+void negativeImage(Mat& image) {
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            image.at<float>(i, j) = 1 - image.at<float>(i, j);
+        }
+    }
+}
+
 //same weights
 void localNeighborhood(Mat* image, Mat* destImage){
     //modified.at<uint8_t>(r,c) = (original.at<uint8_t>(r, c)) /2; // GRAYSCALE
@@ -88,7 +96,7 @@ void gaussianBlur(Mat* image, Mat* destImage){
             double finalBValue = 0;
             double pixelsInMask = 0;
 
-            int neighbourhoodSize = 21;
+            int neighbourhoodSize = 11;
             if (neighbourhoodSize%2 == 0){
                 cout << "The neighbourhoodSize needs to be an odd number" << endl;
                 throw;
@@ -237,68 +245,305 @@ void imageSharpening(Mat* image, Mat* destImage){
     }
 }
 
+
+float average_error(Mat* originalImage, Mat* image){
+    float Rerror = 0;
+    float Gerror = 0;
+    float Berror = 0;
+
+    for (int r = 0; r < (*image).rows; r++) {
+        for (int c = 0; c < (*image).cols; c++) {
+
+            for (int i = 0; i < 3; i++){
+                float tmp = (*originalImage).at<Vec3b>(r,c)[i] - (*image).at<Vec3b>(r,c)[i];
+
+                if (i == 0) Berror += tmp*tmp;
+                if (i == 1) Gerror += tmp*tmp;
+                if (i == 2) Rerror += tmp*tmp;
+
+            }
+        }
+    }
+
+    Berror /= (*image).rows*(*image).cols;
+    Gerror /= (*image).rows*(*image).cols;
+    Rerror /= (*image).rows*(*image).cols;
+
+    return (Rerror+Gerror+Berror)/3;
+}
+
+void getVisualDFT(Mat& image, Mat& destImage, bool dftImage){
+
+    Mat planes[2] = { Mat::zeros(image.size(), CV_32F), Mat::zeros(image.size(), CV_32F) };
+
+    //MAGNITUDE
+    // compute the magnitude and switch to logarithmic scale
+    // => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
+    split(image, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+
+    magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
+    Mat magnitudeImage = planes[0];
+
+    magnitudeImage += Scalar::all(1);                    // switch to logarithmic scale
+
+    if (dftImage) {
+        log(magnitudeImage, magnitudeImage);  // this log messes up the brightness
+    }
+
+    // crop the spectrum, if it has an odd number of rows or columns
+    magnitudeImage = magnitudeImage(Rect(0, 0, magnitudeImage.cols & -2, magnitudeImage.rows & -2));
+
+
+    normalize(magnitudeImage, magnitudeImage, 0, 1, CV_MINMAX); // Transform the matrix with float values into a
+    // viewable image form (float between values 0 and 1).
+
+    destImage = magnitudeImage;
+}
+
+void showImage(String name, Mat image, bool dftImage){
+    Mat imageShow;
+    getVisualDFT(image, imageShow, dftImage);
+    imshow(name, imageShow);
+
+}
+
+// rearrange the quadrants of Fourier image  so that the origin is at the image center
+// its stored again in image
+void rearrangeDFT(Mat& image){
+    int cx = image.cols/2;
+    int cy = image.rows/2;
+
+
+    Mat q0(image, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
+    Mat q1(image, Rect(cx, 0, cx, cy));  // Top-Right
+    Mat q2(image, Rect(0, cy, cx, cy));  // Bottom-Left
+    Mat q3(image, Rect(cx, cy, cx, cy)); // Bottom-Right
+
+    Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+
+    q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+}
+
+void invertDFT(Mat& image, Mat& destImage){
+    Mat inverted;
+    dft(image, inverted, DFT_INVERSE | DFT_REAL_OUTPUT | DFT_SCALE);
+    destImage = inverted;
+}
+
+void readyDFT(String location, Mat& destImage){
+    Mat image = imread(location, CV_LOAD_IMAGE_GRAYSCALE);
+
+    Mat padded;
+    //expand input image to optimal size
+    // if size is an exponential of 2 it is a lot quicker to process
+    int m = getOptimalDFTSize( image.rows );
+    int n = getOptimalDFTSize( image.cols );
+
+    // to do that we need to add 0s to the extra pixels
+    copyMakeBorder(image, padded, 0, m - image.rows, 0, n - image.cols, BORDER_CONSTANT, Scalar::all(0));
+
+
+    Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
+
+    Mat complexImage;
+
+    merge(planes, 2, complexImage);         // Add to the expanded another plane with zeros
+
+    dft(complexImage, complexImage);            // this way the result may fit in the source matrix
+
+    destImage = complexImage;
+
+}
+
+
+
+void gaussianFilter(Size size, Mat& destImage, int centerX, int centerY, float width, float height, float amp = 1.0f) {
+    for (int r = 0; r < size.width; r++) {
+        for (int c = 0; c < size.height; c++) {
+            float x = (((float)r - centerX) * ((float)r - centerX)) / (2.0f * width * width);
+            float y = (((float)c - centerY) * ((float)c - centerY)) / (2.0f * height * height);
+
+            float value = amp * exp(-(x + y));
+            destImage.at<float>(r, c) = value;
+        }
+    }
+
+}
+
+void bandPass(Mat& mask, int centerX, int centerY, float width, float height, int thickness){
+    mask = Mat::ones(mask.size(), CV_32F);
+    ellipse(mask, Point(centerX, centerY), Size(width, height), 0, 0, 360, Scalar(0, 0, 0), thickness);
+}
+
+void lowPass(Mat& mask, int centerX, int centerY, float width, float height){
+    mask = Mat::ones(mask.size(), CV_32F);
+    ellipse(mask, Point(centerX, centerY), Size(width, height), 0, 0, 360, Scalar(0, 0, 0), -1);
+}
+
+void highPass(Mat& mask, int centerX, int centerY, float width, float height){
+    lowPass(mask, centerX, centerY, width, height);
+    negativeImage(mask);
+}
+
+void specialMask(Mat& mask){
+    mask = Mat::ones(mask.size(), CV_32F);
+    int dy = 43;
+    int dx = 77;
+
+    for (int i = -2; i < 3; i++){
+        for (int j = -2; j < 3; j++){
+            if (i == 0 && j == 0) continue;
+            int x = mask.cols/2+dx*i;
+            int y = mask.rows/2+dy*j;
+
+            rectangle(mask, Point(x, y), Point(x, y), Scalar(0,0,0), 5);
+            //rectangle(mask, Point(mask.cols/2-77*2, mask.rows/2-43), Point(mask.cols/2-77*2, mask.rows/2-43), Scalar(0,0,0), 5);
+        }
+    }
+
+}
+
+void createMask(Mat& image, int type){
+    Mat mask;
+    int bSizeY = 30;
+    int bSizeX = bSizeY * ((float)image.cols / (float)image.rows);
+    int sizex = image.cols;
+    int sizey = image.rows;
+
+
+    mask = Mat(Size(sizex, sizey), CV_32F);
+
+    //gaussianFilter(Size(sizex, sizey), mask, image.cols/2, image.rows/2, bSizeX, bSizeY);
+    //bandPass(mask, image.cols/2, image.rows/2, bSizeX, bSizeY, 10);
+    //lowPass(mask, image.cols/2, image.rows/2, bSizeX, bSizeY);
+    //highPass(mask, image.cols/2, image.rows/2, bSizeX, bSizeY);
+    specialMask(mask);
+
+    //normalize(mask, mask, 0, 1, CV_MINMAX);
+
+    showImage("Mask", mask, true);
+
+
+    // do the same than in the image with the 2 channels
+    Mat planes[] = { Mat::zeros(image.size(), CV_32F), Mat::zeros(image.size(), CV_32F) };
+
+    Mat fullMask;
+    planes[0] = mask;
+    // merge the two planes into the single image
+    merge(planes, 2, fullMask);
+
+    // multiply the mask with the image to get the change done
+    mulSpectrums(image, fullMask, image, 0);
+
+    //resize(image, image, image.size()*2, 2, 2);
+    showImage("Mask added to dft", image, true);
+}
+
+Mat createDFT() {
+    Mat dftImage;
+    readyDFT("../PandaNoise.bmp", dftImage);
+
+    rearrangeDFT(dftImage);
+
+    showImage("spectrum magnitude", dftImage, true);
+
+    createMask(dftImage, 1);
+
+    rearrangeDFT(dftImage);
+
+
+    Mat inverted;
+    invertDFT(dftImage, inverted);
+
+    return inverted;
+
+}
+
 int main( int argc, char** argv ) {
     Mat original;
+    Mat noisy;
     Mat modified;
     Mat modified2;
     Mat modified3;
     Mat modified4;
     Mat modified5;
+    Mat modified6;
 
 
     // Read the file
-    original = imread("../PandaNoise.bmp", CV_LOAD_IMAGE_COLOR);
+    original = imread("../PandaOriginal.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+    noisy = imread("../PandaNoise.bmp", CV_LOAD_IMAGE_COLOR);
     modified = imread("../PandaNoise.bmp", CV_LOAD_IMAGE_COLOR);
     modified2 = imread("../PandaNoise.bmp", CV_LOAD_IMAGE_COLOR);
     modified3 = imread("../PandaNoise.bmp", CV_LOAD_IMAGE_COLOR);
     modified4 = imread("../PandaNoise.bmp", CV_LOAD_IMAGE_COLOR);
     modified5 = imread("../PandaNoise.bmp", CV_LOAD_IMAGE_COLOR);
+    modified6 = imread("../PandaNoise.bmp", CV_LOAD_IMAGE_GRAYSCALE);
 
-    if(! original.data ){                              // Check for invalid input
+    if(! noisy.data ){                              // Check for invalid input
         cout <<  "Could not open or find the image" << std::endl ;
         return -1;
     }
 
 
     namedWindow("ORIGINAL", CV_WINDOW_AUTOSIZE);// Create a window for display.
-    namedWindow("MODIFIED", CV_WINDOW_AUTOSIZE);// Create a window for display.
-    namedWindow("MODIFIED2", CV_WINDOW_AUTOSIZE);// Create a window for display.
+    namedWindow("NOISY", CV_WINDOW_AUTOSIZE);// Create a window for display.
+    namedWindow("LOCAL NEIGHBOURHOOD", CV_WINDOW_AUTOSIZE);// Create a window for display.
+    namedWindow("GAUSSIAN BLUR", CV_WINDOW_AUTOSIZE);// Create a window for display.
     namedWindow("EDGE PRESERVING", CV_WINDOW_AUTOSIZE);// Create a window for display.
     namedWindow("IMAGE SHARPENING", CV_WINDOW_AUTOSIZE);// Create a window for display.
     namedWindow("EDGE FILTERING", CV_WINDOW_AUTOSIZE);// Create a window for display.
+    namedWindow("DFT", CV_WINDOW_AUTOSIZE);// Create a window for display.
+    namedWindow("Mask added to dft", CV_WINDOW_AUTOSIZE);// Create a window for display.
 
     imshow("ORIGINAL", original);
-    //imshow("MODIFIED", modified);
+    imshow("NOISY", noisy);
 
 
     // put each image next to each other
     moveWindow("ORIGINAL", 0, 0);            // put window in certain position in the screen
-    moveWindow("MODIFIED", original.cols, 0);            // put window in certain position in the screen
-    moveWindow("MODIFIED2", original.cols*2, 0);            // put window in certain position in the screen
-    moveWindow("EDGE PRESERVING", 0, original.rows+50);            // put window in certain position in the screen
-    moveWindow("IMAGE SHARPENING", original.cols, original.rows+50);            // put window in certain position in the screen
-    moveWindow("EDGE FILTERING", original.cols*2, original.rows+50);            // put window in certain position in the screen
+    moveWindow("NOISY", noisy.cols, 0);           // put window in certain position in the screen
+    moveWindow("LOCAL NEIGHBOURHOOD", noisy.cols*2, 0);            // put window in certain position in the screen
+    moveWindow("GAUSSIAN BLUR", 0, noisy.rows+50);            // put window in certain position in the screen
+    moveWindow("EDGE PRESERVING", noisy.cols, noisy.rows+50);            // put window in certain position in the screen
+    moveWindow("IMAGE SHARPENING", noisy.cols*2, noisy.rows+50);            // put window in certain position in the screen
+    moveWindow("EDGE FILTERING", 0, (noisy.rows+50)*2);            // put window in certain position in the screen
+    moveWindow("DFT", noisy.cols, (noisy.rows+50)*2);            // put window in certain position in the screen
+    moveWindow("Mask added to dft", noisy.cols*2, (noisy.rows+50)*2);            // put window in certain position in the screen
 
     //imwrite("filename.jpg", image); //save image
 
-    //absdiff(modified, original, modified);   //compare 2 images and tell the difference, result: threshold image https://www.youtube.com/watch?v=X6rPdRZzgjg&index=3&list=PLo1wvPF7fMxQ_SXibg1azwBfmTFn02B9O
+    //absdiff(modified, noisy, modified);   //compare 2 images and tell the difference, result: threshold image https://www.youtube.com/watch?v=X6rPdRZzgjg&index=3&list=PLo1wvPF7fMxQ_SXibg1azwBfmTFn02B9O
 
 
     //make images blurry taking random noise away
-    localNeighborhood(&original, &modified);
-    gaussianBlur(&original, &modified2);
-    edgePreserving(&original, &modified3);
-    imageSharpening(&original, &modified4);
-    edgeFiltering(&original, &modified5);
+    localNeighborhood(&noisy, &modified);
+    gaussianBlur(&noisy, &modified2);
+    edgePreserving(&noisy, &modified3);
+    imageSharpening(&noisy, &modified4);
+    edgeFiltering(&modified2, &modified5);
+    Mat modified7 = createDFT();
 
     // show images
-    imshow("ORIGINAL", original);
-    imshow("MODIFIED", modified);
-    imshow("MODIFIED2", modified2);
+    imshow("LOCAL NEIGHBOURHOOD", modified);
+    imshow("GAUSSIAN BLUR", modified2);
     imshow("EDGE PRESERVING", modified3);
     imshow("IMAGE SHARPENING", modified4);
     imshow("EDGE FILTERING", modified5);
+    showImage("DFT", modified7, false);
 
+
+    cout << "LOCAL NEIGHBOURHOOD: " << average_error(&original, &modified) << endl;
+    cout << "GAUSSIAN BLUR: " << average_error(&original, &modified2) << endl;
+    cout << "EDGE PRESERVING: " << average_error(&original, &modified3) << endl;
+    cout << "IMAGE SHARPENING: " << average_error(&original, &modified4) << endl;
+    cout << "EDGE FILTERING: " << average_error(&original, &modified5) << endl;
+    cout << "DFT: " << average_error(&original, &modified6) << endl;
 
     //infinite loop
     while (1) {
